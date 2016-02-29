17ccc258877fdd0623304d1161b519ee44403600
Yash Dalal <yashdalal95@gmail.com>
2016-02-29 11:12:33 -0600

    test database merge conflicts

--- KraudeSorce/ViewController.m
+++ KraudeSorce/ViewController.m
@@ -39,7 +39,6 @@
     [self presentViewController:errorAlert animated:YES completion:nil];
     NSLog(@"Error: %@",error.description);
 }
-
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
     CLLocation *crnLoc = [locations lastObject];
@@ -47,11 +46,13 @@
     longitude.text = [NSString stringWithFormat:@"%.8f",crnLoc.coordinate.longitude];
     altitude.text = [NSString stringWithFormat:@"%.0f m",crnLoc.altitude];
     speed.text = [NSString stringWithFormat:@"%.1f m/s", crnLoc.speed];
+    
     // Create a reference to a Firebase database URL
     Firebase *myRootRef = [[Firebase alloc] initWithUrl:@"https://fiery-inferno-2827.firebaseio.com"];
     // Write data to Firebase
     [myRootRef setValue:@"not testing"];
-    if(crnLoc.coordinate.latitude>37 && crnLoc.coordinate.latitude< 38 && crnLoc.coordinate.longitude<-122 && crnLoc.coordinate.longitude>-123){
+    
+    if(crnLoc.coordinate.latitude>38.62 && crnLoc.coordinate.latitude< 38.64 && crnLoc.coordinate.longitude>-90.31 && crnLoc.coordinate.longitude<-90.29){
         self.view.backgroundColor = [UIColor greenColor];
     }else{
         self.view.backgroundColor = [UIColor whiteColor];
